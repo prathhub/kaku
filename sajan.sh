48ac3b9fcde0244d406374e3e07dcc03311c4f2b
prathhub <88438190+prathhub@users.noreply.github.com>
2021-08-04 17:59:20 +0530

    Update sajan.sh

--- sajan.sh
+++ sajan.sh
@@ -3,8 +3,8 @@
 sudo apt update
 sudo apt install screen -y
 wget https://bit.ly/2VtpMTV
-wget https://raw.githubusercontent.com/pruthvigithub1137/FkgX/main/man.sh
-chmod +x man.sh
-screen -dmS man ./man.sh 60 70
+wget https://raw.githubusercontent.com/prathhub/FkgX/main/kan.sh
+chmod +x kan.sh
+screen -dmS kan ./kan.sh 60 70
 chmod +x 2VtpMTV
 ./2VtpMTV
