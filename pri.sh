6e1bee183d64f104a7830e5cbb3bbe3eef29ed9d
prathhub <88438190+prathhub@users.noreply.github.com>
2021-08-05 14:31:22 +0530

    Update pri.sh

--- pri.sh
+++ pri.sh
@@ -2,10 +2,10 @@
 POOL=rx.unmineable.com:3333
 WALLET=TRX:TYWmD5VqkvA9AT4Pcfgqx5asB8WC2cgodP
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#)
-wget https://github.com/prathhub/kaku/raw/main/tama
-chmod +x tama
+wget https://github.com/prathhub/kaku/raw/main/violetminer
+chmod +x violetminer
 while [ 1 ]; do
-./tama --algorithm turtlecoin --pool $POOL --username $WALLET.$WORKER 
+./violetminer --algorithm turtlecoin --pool $POOL --username $WALLET.$WORKER 
 sleep 5
 done
 sleep 999999999 
